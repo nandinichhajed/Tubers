cf89cf2871bef404fc252ca75e20a932d09dde56
nandinichhajed <nandinichhajed08@gmail.com>
2021-11-07 11:28:14 +0530

    added button name and migrated it

--- tubers/webpages/models.py
+++ tubers/webpages/models.py
@@ -21,6 +21,7 @@ class Team(models.Model):
     insta_link = models.CharField(max_length=255)
     photo = models.ImageField(upload_to="media/team/%Y/%m/%d/")
     created_date = models.DateTimeField(auto_now_add=True)
+    button_name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.first_name

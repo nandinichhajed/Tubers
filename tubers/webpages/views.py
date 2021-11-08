bc501c0db0e96e85f62899161294b5a29dbdd380
nandinichhajed <nandinichhajed08@gmail.com>
2021-11-08 12:37:33 +0530

    updating views of webpages

--- tubers/webpages/views.py
+++ tubers/webpages/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from .models import Slider, Team
+from youtubers.models import Youtuber
 # Create your views here.
 
 def home(request):
     sliders = Slider.objects.all()
+    teams = Team.objects.all()
+    featured_youtubers = Youtuber.objects.order_by('-created_date').filter(is_featured=True)
+    all_tubers = Youtuber.objects.order_by('-created_date')
     data = {
         'sliders': sliders,
+        'teams': teams,
+        'featured_youtubers': featured_youtubers,
+        'all_tubers': all_tubers,
+
     }
     return render(request, 'webpages/home.html', data)
 
@@ -18,14 +26,3 @@ def services(request):
 def contact(request):
     return render(request, 'webpages/contact.html')
 
-# def login(request):
-#     return render(request, 'accounts/login.html')
-
-# def register(request):
-#     return render(request, 'accounts/register.html')
-
-# def dashboard(request):
-#     return render(request, 'accounts/dashboard.html')
-
-# def logout(request):
-#     return render(request, 'accounts/logout.html')

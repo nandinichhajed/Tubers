7bc36a6d4528de397e796e5d7fc0cc43e341b767
nandinichhajed <nandinichhajed08@gmail.com>
2021-11-08 13:38:57 +0530

    updated admin views and urls

--- tubers/youtubers/urls.py
+++ tubers/youtubers/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.youtubers, name='youtubers'),
-    path('<int:id>', views.youtuber, name='youtuber'),
+    path('<int:id>', views.youtubers, name='youtubers'),
     path('search', views.search, name='search'),
 ]

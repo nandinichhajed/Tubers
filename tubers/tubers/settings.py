fbb248af4ab1d74d4e48bfb5fb57e24418cb8db6
nandinichhajed <nandinichhajed08@gmail.com>
2021-11-12 14:04:41 +0530

    creating app hiretuber

--- tubers/tubers/settings.py
+++ tubers/tubers/settings.py
@@ -32,6 +32,7 @@ LOGIN_REDIRECT_URL = 'dashboard'
 # Application definition
 
 INSTALLED_APPS = [
+    'hiretuber.apps.HiretuberConfig',
     'accounts.apps.AccountsConfig',
     'youtubers.apps.YoutubersConfig',
     'webpages.apps.WebpagesConfig',
--- tubers/tubers/urls.py
+++ tubers/tubers/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('webpages.urls')),
     path('youtubers/', include('youtubers.urls')),
+    path('hiretuber/', include('hiretuber.urls')),
     path('accounts/', include('accounts.urls')),
     path('socialaccounts/', include('allauth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

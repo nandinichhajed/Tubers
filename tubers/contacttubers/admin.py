8b11e87c749624abe5fd62cd0a68782eb67e7646
nandinichhajed <nandinichhajed08@gmail.com>
2021-11-14 16:57:52 +0530

    making changes in models and updating migrations

--- tubers/contacttubers/admin.py
+++ tubers/contacttubers/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Contact
 
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id', 'full_name','email', 'company_name')
+    list_display = ('id', 'full_name','email')
     list_display_links = ('full_name', 'id')
 
 
--- tubers/contacttubers/models.py
+++ tubers/contacttubers/models.py
@@ -6,11 +6,11 @@ class Contact(models.Model):
     full_name = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
     email = models.CharField(max_length=200)
-    company_name = models.CharField(max_length=100, blank=True)
+    company_name = models.CharField(max_length=100)
     subject = models.TextField()
     message = models.TextField()
     user_id = models.IntegerField(blank=True)
     created_date = models.DateTimeField(blank=True, default=datetime.now)
 
     def __str__(self):
-        return self.company_name
+        return self.email

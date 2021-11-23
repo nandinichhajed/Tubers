4edaa8158d318b2b1b3aa07975e70dbc96efe55f
nandinichhajed <nandinichhajed08@gmail.com>
2021-11-23 18:13:29 +0530

    creating admin file

--- tubers/hiretubers/admin.py
+++ tubers/hiretubers/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Hiretuber
 # Register your models here.
+class HiretuberAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'email', 'tuber_name')
+    list_display_links = ('id', 'first_name')
+    
+admin.site.register(Hiretuber, HiretuberAdmin)

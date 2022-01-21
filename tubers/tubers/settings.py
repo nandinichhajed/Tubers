9713d57a9cec533b9dae98731e7129ad9d921cfc
nandinichhajed <nandinichhajed08@gmail.com>
2022-01-21 11:11:26 +0530

    updating settings

--- tubers/tubers/settings.py
+++ tubers/tubers/settings.py
@@ -18,6 +18,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']

f2d2be5d60256ebde4c15d79d56a9ac0d1b61ecf
nandinichhajed <nandinichhajed08@gmail.com>
2022-02-06 14:20:28 +0530

    depoly2

--- tubers/tubers/settings.py
+++ tubers/tubers/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',cast=bool)
 
-ALLOWED_HOSTS = ['applicationtubers.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 LOGIN_REDIRECT_URL = 'dashboard'
 

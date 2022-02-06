bc0b0eae0999a58c28c756004f754118a3008f38
nandinichhajed <nandinichhajed08@gmail.com>
2022-02-06 17:27:20 +0530

    changing settings

--- tubers/settings.py
+++ tubers/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["applicationtubers.herokuapp.com", "localhost"]
 
@@ -163,7 +163,7 @@ STATICFILES_DIRS = [
 
 django_heroku.settings(locals())
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

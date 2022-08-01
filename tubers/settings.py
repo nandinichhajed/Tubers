b269a8d37811d648ad3a39e55ffebdad71173448
Nandini Chhajed <nandinichhajed08@gmail.com>
2022-08-02 00:58:55 +0530

    Whitenoice

--- tubers/settings.py
+++ tubers/settings.py
@@ -164,7 +164,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'tubers/static')
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR,'media')
